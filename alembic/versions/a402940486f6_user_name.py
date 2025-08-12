590882bf04ec57c4fa1a65717db28a865b022040
Ran Ram <p0038974@ness-tech.co.il>
2025-08-12 11:10:51 +0300

    Username for alembic

---
+++ alembic/versions/a402940486f6_user_name.py
@@ -0,0 +1,24 @@
+"""user name
+
+Revision ID: a402940486f6
+Revises: cf36412b4ea9
+Create Date: 2025-08-12 11:10:11.540901
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a402940486f6'
+down_revision = 'cf36412b4ea9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
